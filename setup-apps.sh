3c2dbc7bf92227ecbbc1314e06145339ac947d3b
Pierre Besson <pbesson@ippon.fr>
2016-04-25 10:55:32 +0200

    update setup entities scripts

--- setup-apps.sh
+++ setup-apps.sh
@@ -2,7 +2,7 @@
 
 #Define the folders in which we will run `yo jhipster`
 
-apps=("gateway" "app1" "app2" "app3")
+apps=("gateway" "app1" "app2" "app3" "app4")
 for app in "${apps[@]}";
 do
     ( cd $app && yo jhipster --force --with-entities ) 
