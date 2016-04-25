b87d2f3c062dd8ff5b4ff6fec1856b82b6ed61e0
Pierre Besson <pbesson@ippon.fr>
2016-04-25 18:08:23 +0200

    correct build script

--- build-apps.sh
+++ build-apps.sh
@@ -3,8 +3,8 @@
 #Define the folders in which we will run `mvn package docker:build -DskipTests=true`
 
 
-apps=("gateway" "app1" "app2" "app3" "apps4")
+apps=("gateway" "app1" "app2" "app3" "app4")
 for app in "${apps[@]}";
 do
-    ( cd $app && mvn package docker:build -DskipTests=true ) 
+    ( cd $app && mvn package -Pprod docker:build -DskipTests ) 
 done
